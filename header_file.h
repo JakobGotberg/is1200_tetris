9806914b1c2054e48525ac7f38a6bd90f3e0fd05
JakobGotberg <71640271+JakobGotberg@users.noreply.github.com>
2021-03-03 18:07:14 +0100

    Add files via upload

---
+++ header_file.h
@@ -0,0 +1,143 @@
+/* mipslab.h
+   Header file for all labs.
+   This file written 2015 by F Lundevall
+   Some parts are original code written by Axel Isaksson
+
+   Latest update 2015-08-28 by F Lundevall
+
+   For copyright and licensing, see file COPYING */
+
+/* Declare display-related functions from mipslabfunc.c */
+void display_image(int x, const uint8_t *data);
+void display_init(void);
+void display_string(int line, char *s);
+void display_update(void);
+uint8_t spi_send_recv(uint8_t data);
+
+/* Declare lab-related functions from mipslabfunc.c */
+char * itoaconv( int num );
+void labwork(void);
+int nextprime( int inval );
+void quicksleep(int cyc);
+void tick( unsigned int * timep );
+
+/* Declare display_debug - a function to help debugging.
+
+   After calling display_debug,
+   the two middle lines of the display show
+   an address and its current contents.
+
+   There's one parameter: the address to read and display.
+
+   Note: When you use this function, you should comment out any
+   repeated calls to display_image; display_image overwrites
+   about half of the digits shown by display_debug.
+*/
+void display_debug( volatile int * const addr );
+
+/* Declare bitmap array containing font */
+extern const uint8_t const font[128*8];
+/* Declare bitmap array containing icon */
+extern const uint8_t const icon[128];
+/* Declare text buffer for display output */
+extern char textbuffer[4][16];
+
+/* Declare functions written by students.
+   Note: Since we declare these functions here,
+   students must define their functions with the exact types
+   specified in the laboratory instructions. */
+/* Written as part of asm lab: delay, time2string */
+void delay(int);
+void time2string( char *, int );
+/* Written as part of i/o lab: getbtns, getsw, enable_interrupt */
+int getbtns(void);
+int getsw(void);
+void enable_interrupt(void);
+
+/********************************/
+//project functions, global variables och arrays
+/********************************/
+extern char shape;
+extern uint8_t row;
+extern uint8_t block;
+extern uint8_t downCollision;
+extern uint8_t leftCollision;
+extern uint8_t rightCollision;
+extern uint8_t rotationPosition;
+
+extern uint8_t gameover;
+extern uint8_t score;
+
+extern uint8_t dis_arr[512];
+extern uint8_t* dis_ptr;
+extern uint8_t logic_array[200];
+extern uint8_t* log_ptr;
+extern uint8_t collision_array[20][10];
+
+void display_frame(void);
+void display_print(void); 
+void display_testing(void); // trash
+void timer_setup( void );
+
+void fall_down(void);
+void row_check(void);
+void row_clear(uint8_t _row);
+
+void length_test(void); // trash
+
+void pixel_set(unsigned int x, unsigned int y);
+void pixel_clr(unsigned int x, unsigned int y);
+
+void logic_to_pixel_set( uint8_t row, uint8_t block );
+void logic_to_pixel_clr( uint8_t row, uint8_t block );
+
+void display_shape(void);
+void delete_shape(void);
+
+void display_shape_T(void);
+void display_shape_L(void);
+void display_shape_J(void);
+void display_shape_Z(void);
+void display_shape_S(void);
+void display_shape_I(void);
+void display_shape_O(void);
+
+void delete_shape_T(void);	
+void delete_shape_L(void);
+void delete_shape_J(void);
+void delete_shape_Z(void);
+void delete_shape_S(void);
+void delete_shape_I(void);
+void delete_shape_O(void);
+
+uint8_t collision_check(uint8_t row, uint8_t block);
+void collision_check_per_shape(char direction);
+
+void collision_check_right_shape_T(void);
+void collision_check_left_shape_T(void);
+void collision_check_down_shape_T(void);
+
+void collision_check_down_shape_L(void);
+void collision_check_left_shape_L(void);
+void collision_check_right_shape_L(void);
+
+void collision_check_right_shape_J(void);
+void collision_check_left_shape_J(void);
+void collision_check_down_shape_J(void);
+
+void collision_check_right_shape_Z(void);
+void collision_check_left_shape_Z(void);
+void collision_check_down_shape_Z(void);
+
+void collision_check_right_shape_S(void);
+void collision_check_left_shape_S(void);
+void collision_check_down_shape_S(void);
+
+void collision_check_right_shape_I(void);
+void collision_check_left_shape_I(void);
+void collision_check_down_shape_I(void);
+
+void collision_check_right_shape_O(void);
+void collision_check_left_shape_O(void);
+void collision_check_down_shape_O(void);
+
